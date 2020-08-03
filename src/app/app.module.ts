8bcfad864528b7ba64bce00d14dbc15534c70c07
Mihai-Andrei Neacsu <crunck78@googlemail.com>
2020-08-04 01:46:28 +0200

    add menu header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SlideshowComponent
+    SlideshowComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
