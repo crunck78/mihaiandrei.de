e9c4d2932cbd19c5d8fc13f996d6fe016ff245da
Mihai-Andrei Neacsu <crunck78@googlemail.com>
2020-08-03 22:29:26 +0200

    add slide-show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mihaiandrei';
+export class AppComponent implements OnInit {
+  imgs = ["laptop.jpg", "laptop2.jpg"];
+  currentImg = 0;
+  showImg = true;
+  ngOnInit() {
+    this.updateImg();
+  }
+
+  updateImg() {
+    setInterval(() =>{
+      this.currentImg++;
+      this.currentImg = this.currentImg % this.imgs.length;
+      this.showImg = false;
+      setInterval(() => {
+        this.showImg = true;
+      }, 100);
+    }, 8000);
+  }
 }
