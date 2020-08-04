4f7116d52c3edeab4bc9f1f8ec7b0cfcc750dd4d
Mihai-Andrei Neacsu <crunck78@googlemail.com>
2020-08-04 02:41:45 +0200

    add darkmode

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ImprintComponent } from './imprint/imprint.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SlideshowComponent },
+  { path: 'imprint', component: ImprintComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
