327d6e5e0e7592f3293206ba674b4d53daacad11
Mihai-Andrei Neacsu <crunck78@googlemail.com>
2020-08-04 13:22:15 +0200

    add imprint, privacypolicy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ImprintComponent } from './imprint/imprint.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
+import { PrivacypolicyComponent } from './privacypolicy/privacypolicy.component';
 
 const routes: Routes = [
   { path: '', component: SlideshowComponent },
-  { path: 'imprint', component: ImprintComponent }
+  { path: 'imprint', component: ImprintComponent },
+  { path: 'privacypolicy', component: PrivacypolicyComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { MenuComponent } from './menu/menu.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacypolicyComponent } from './privacypolicy/privacypolicy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SlideshowComponent,
     MenuComponent,
-    ImprintComponent
+    ImprintComponent,
+    PrivacypolicyComponent
   ],
   imports: [
     BrowserModule,
